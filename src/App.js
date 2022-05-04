7334fc04a9cb74336b81963882bbdc59cd98c241
David Rajcher (Codaisseur) <davidrajchercodaisseur@Davids-MBP.codaisseur.local>
2022-05-04 10:34:34 +0200

    squares

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import './App.css';
+import "./App.css";
 import { Banner, Destinations, Footer, Navbar } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
-      <Banner/>
-      <Destinations/>
-      <Footer/>
+    <div className='App'>
+      {/* <Navbar />
+      <Banner />
+      <Destinations />
+      <Footer /> */}
+
+      <Squares />
     </div>
   );
 }
---
+++ src/components/squares/Squares.jsx
@@ -0,0 +1,14 @@
+.square-container {
+    height: 100%;
+    width: 50%;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: wrap-reverse;
+    justify-content: center;
+    align-items: center;
+  }
+  
+  .square {
+    width: 100px;
+    height: 100px;
+  }
